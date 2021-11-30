7454bd5a41ee94011a48d66e94121914360e548f
duck <godqhr2256@gmail.com>
2021-11-30 17:21:00 +0900

    antd 설정

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a63cc10cbe4bdcde"; }
+/******/ 		__webpack_require__.h = function() { return "81f4c01f5362b042"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
