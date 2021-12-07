6d472f4ed21d2a74fbf2c6d5ae416e7d741a55d5
duck <godqhr2256@gmail.com>
2021-12-07 15:11:06 +0900

    회원가입페이지 에러수정

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,53 +1,36 @@
-import React,{useState, useCallback, useMemo} from 'react'
-import {Form,Input,Button} from 'antd';
-import Link from 'next/link'
-import styled from 'styled-components';
+import React, { useCallback } from 'react';
+import { Button, Form, Input } from 'antd';
+import Link from 'next/link';
 
-const ButtonWrapper = styled.div`
-    margin-top : 10px;
-`;
+import useInput from '../hooks/useInput';
 
-const LoginForm = ({setIsLoggedin}) =>{
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
+const LoginForm = () => {
+  const [id, onChangeId] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const onSubmitForm = useCallback(() => {
+    console.log({
+      id, password,
+    });
+  }, [id, password]);
 
-    const onChangeId = useCallback((e) =>{
-        setId(e.target.value);
-    },[]); // 컴포넌트에 props로 들어가기 때문에 usecallback을 이용하여 최적화해줌.
-
-    const onChangePassword = useCallback((e) =>{
-        setPassword(e.target.value);
-    },[]);
-
-    /*const style = useMemo(() => ({marginTop : 10}), []); // 리렌더링 성능 최적화를 위함. */
-
-    const onSubmitForm = useCallback(()=>{ // 컴포넌트에 넣기 때문에 useCallback을 사용함.
-        console.log(id, password);
-        setIsLoggedin(true);
-    },[id, password])
-
-    const FormWrapper = styled(Form)`
-    padding : 10px;
-    `
-
-    return(
-        <FormWrapper onFinish={onSubmitForm}>  {/* onFinish는 이미 prevent.default가 적용되어있음.*/}
-            <div>
-                <label htmlFor ="user-id">아이디</label>
-                <br />
-                <Input name ="user-id" value={id} onChange = {onChangeId} required />
-            </div>
-            <div>
-                <label htmlFor ="user-password">비밀번호</label>
-                <br />
-                <Input name ="user-password" type="password" value={password} onChange = {onChangePassword} required />
-            </div>
-            <ButtonWrapper /*style={style}*/>
-                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
-                <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-            </ButtonWrapper>
-        </FormWrapper>
-    );
-}
+  return (
+    <Form onFinish={onSubmitForm} style={{ padding: '10px' }}>
+      <div>
+        <label htmlFor="user-id">아이디</label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label>
+        <br />
+        <Input name="user-password" value={password} onChange={onChangePassword} type="password" required />
+      </div>
+      <div style={{ marginTop: '10px' }}>
+        <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+        <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+      </div>
+    </Form>
+  );
+};
 
 export default LoginForm;
